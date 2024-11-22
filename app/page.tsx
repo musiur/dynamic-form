06eb47876e24f6f1fa6155b67f4fbbd79f7d8e04
musiur <musiur.opu@gmail.com>
2024-11-23 03:37:14 +0600

    ready for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,136 @@
-import Image from "next/image";
+"use client"
+import { FormX, FormX__TYPE_Structure, FormX__TYPE_Field, FormX__SchemaBuilder } from "@/components/form-x/form-x";
+import { z } from "zod";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const FormFields: FormX__TYPE_Field[] = [
+  {
+    id: 1,
+    type: "text",
+    name: "username",
+    label: "Username",
+    placeholder: "Enter your username",
+    description: "This is your public display name.",
+    validation: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+    defaultValue: "John Doe"
+  },
+  {
+    id: 2,
+    type: "text",
+    name: "email",
+    label: "Email",
+    placeholder: "Enter your email",
+    validation: z.string().email({
+      message: "Please enter a valid email address.",
+    }),
+    defaultValue: "musiur.opu@gmail.com"
+  },
+  {
+    id: 3,
+    type: "password",
+    name: "password",
+    label: "Password",
+    placeholder: "Enter your password",
+    validation: z.string().min(8, {
+      message: "Password must be at least 8 characters.",
+    }),
+    defaultValue: "12345678"
+  },
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  {
+    id: 4,
+    type: "image",
+    name: "avatar",
+    label: "Avatar",
+    defaultValue: "https://via.placeholder.com/150",
+    validation: z.instanceof(File)
+  },
+  {
+    id: 5,
+    type: "image",
+    name: "images",
+    label: "Images",
+    multiple: true,
+    defaultValue: [],
+    validation: z.array(z.instanceof(File))
+  },
+  {
+    id: 6,
+    type: "checkbox",
+    name: "terms",
+    label: "Accept terms and conditions",
+    description: "By accepting, you agree to the terms and conditions.",
+    validation: z.boolean(),
+    defaultValue: false
+  },
+]
+export const [HomeFormSchema, HomeFormDefaultValues, HomeFormZodType] = FormX__SchemaBuilder(FormFields)
+const FormStructure: FormX__TYPE_Structure = {
+  fields: FormFields,
+  submission: {
+    toast: true,
+    submitHandler: (data) => { console.log(data) }
+  },
+
 }
+
+const Home = () => {
+  return (
+    <div className="container section">
+      <div className="bg-gray-200 p-8 rounded-lg space-y-8">
+        <h1 className="text-4xl font-bold">Testing custom form</h1>
+        <FormX structure={nestedFormStructure} className="max-w-[400px] p-4 shadow-xl rounded-lg border bg-white" />
+
+        <FormX structure={FormStructure} className="max-w-[400px] p-4 shadow-xl rounded-lg border bg-white" />
+      </div>
+    </div>
+  )
+}
+export default Home;
+
+const nestedFormStructure: FormX__TYPE_Structure = {
+  fields: [
+    {
+      id: 1,
+      type: "text",
+      name: "username",
+      label: "Username",
+      validation: z.string().min(2, "Username must be at least 2 characters"),
+    },
+    {
+      id: 2,
+      type: "group",
+      name: "address",
+      label: "Address",
+      fields: [
+        {
+          id: 3,
+          type: "text",
+          name: "street",
+          label: "Street",
+          validation: z.string().min(1, "Street is required"),
+        },
+        {
+          id: 4,
+          type: "group",
+          name: "contact",
+          label: "Contact",
+          fields: [
+            {
+              id: 5,
+              type: "text",
+              name: "email",
+              label: "Email",
+              validation: z.string().email("Invalid email address"),
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  submission: {
+    toast: true,
+    submitHandler: (data) => console.log(data)
+  }
+};
